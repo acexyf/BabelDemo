ed7a706370e0cf7e4fb4cff551039ca30f5f3b86
corner <corner_scofield@163.com>
2020-11-25 17:35:25 +0800

    优化

--- demo0/index.js
+++ demo0/index.js
@@ -1,14 +1,27 @@
-var babelCore = require('@babel/core')
-var sourceCode = `let fn = (num) => num + 2`
+var babelCore = require("@babel/core");
+var sourceCode = `let fn = (num) => num + 2`;
 
 var options = {
-    presets: []
-}
+  //是否生成解析的代码
+  code: true,
+  //是否生成抽象语法树
+  ast: true,
+  //是否生成sourceMap
+  sourceMaps: true,
+  plugins: [],
+  presets: [],
+};
 
-var result = babelCore.transform(sourceCode, options)
-
-console.log('source code：')
-console.log(sourceCode)
-console.log('==========')
-console.log('result code：')
-console.log(result.code)
+babelCore.transform(sourceCode, options, function (err, result) {
+  console.log("source code：");
+  console.log(sourceCode);
+  console.log("==========");
+  console.log("result code：");
+  console.log(result.code);
+  console.log("==========");
+  console.log("result map：");
+  console.log(result.map);
+  console.log("==========");
+  console.log("result ast：");
+  console.log(result.ast);
+});
