80d7ba46fb600087ac44380ddf101149b6dab594
corner <corner_scofield@163.com>
2020-12-02 21:09:54 +0800

    完结

--- demo7/index.js
+++ demo7/index.js
@@ -2,6 +2,18 @@
   return value > 9;
 });
 
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  say() {
+    console.log(`my name is：${this.name}`);
+  }
+}
+const tom = new Person("tom");
+tom.say();
+
+
 let promise = new Promise((resolve, reject) => {
   resolve(1);
 });
